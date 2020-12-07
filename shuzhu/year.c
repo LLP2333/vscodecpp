73dabf5b502911e918409a37b128faf664495279
LLP2333 <nasa105@foxmail.com>
2020-12-07 15:18:33 +0800

    origin

--- shuzhu/year.c
+++ shuzhu/year.c
@@ -4,13 +4,22 @@
 #define MONTHS 12
 int main(void)
 {
-    int day[MONTHS] ={31,28,31,30,31,30,31,31,30,31,30,31};
-    int month;
+int day[2][MONTHS] =   {{31,28,31,30,31,30,31,31,30,31,30,31},
+                        {31,29,31,30,31,30,31,31,30,31,30,31}};
+                                    
+    int year,month;
     do{
-        printf("input a month:");
-        scanf("%d",&month);
+        printf("please input year,month:");
+        scanf("%d,%d",&year,&month);//yearºÍmonthÓÃ¶ººÅ¸ô¿ª
     }while(month<1||month>12);
-    printf("The number of days  is %d\n",day[month-1]);
+    if ((0 == year % 4 && 0 != year % 100) || 0 == year % 400)
+    {
+    printf("The number of days  is %d\n",day[1][month-1]);
+    }
+    else
+    {
+        printf("The number of days  is %d\n",day[2][month-1]);
+    }
     system("pause");
     return 0;
 }
