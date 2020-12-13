2510fe5209fdfad50cf0610c158fae26e673898a
LLP2333 <nasa105@foxmail.com>
2020-12-13 09:36:21 +0800

    origin

--- newScore.c
+++
@@ -1,8 +0,0 @@
-/*从键盘输入某班学生某门课的成绩（已知每班人数最多不超过40人，具体人数由键盘输入，
-尝试编程计算其平均值*/
-#include<stdio.h>
-#include<stdlib.h>
-#define N 40
-int Average(int score[],int n);
-void ReadScore(int score[],int n);
-int 
--- shuzhu/README.md
+++
@@ -1,2 +0,0 @@
-
-# today we need to learn "Array"C语言第十三章会考，计算机二级
