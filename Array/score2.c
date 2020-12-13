9f76f8a00e603b064ede9663741e36a4a6ba165b
LLP2333 <nasa105@foxmail.com>
2020-12-13 16:34:21 +0800

    origin

--- Array/score2.c
+++ Array/score2.c
@@ -1,17 +1,23 @@
 /*从键盘输入某班学生某门课的成绩（每班人数最多不超过40人）
-当输入为负值，表示输入结束 ，试着计算并打印最高分*/
+当输入为负值，表示输入结束 ，输出总人数，平均成绩和最高分
+注：平均成绩取整数*/
 #include<stdio.h>
 #include<stdlib.h>
 #define N 40
 int ReadScore(int score[]);
 int FindMax(int score[],int n);
+int Average(int score[],int n);
 
 int main()
 {
     int score[N],max,n;//n表示人数
+    int average;
     n= ReadScore(score);
     printf("Total students are %d\n",n);
+    average=Average(score,n);
+    printf("The Average score is %d\n",average);
     max =FindMax(score,n);
+
     printf("The hightest score is %d\n",max);
     system("pause");
     return 0;
@@ -47,4 +53,21 @@ int FindMax(int score[],int n)
         }
     }
     return max;
+}
+int Average(int score[],int n)
+{
+    int i,sum=0;
+    for(i=0;i<n;i++)
+    {
+        sum=sum +score[i];
+
+    }
+    if(n>0)
+    {
+        return sum/n;
+    }
+    else 
+    {
+        return -1;
+    }
 }
---
+++ Recursion and Iteration/1.c
@@ -0,0 +1,3 @@
+//该程序用来演示如何使用递归函数
+#include<stdio.h>
+#include<stdlib.h>
