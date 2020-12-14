a677f20906fa82f971644b550f04aa31d10fe170
LLP2333 <nasa105@foxmail.com>
2020-12-14 15:40:42 +0800

    origin

--- Array/year.c
+++ Array/year.c
@@ -10,7 +10,7 @@ int day[2][MONTHS] =   {{31,28,31,30,31,30,31,31,30,31,30,31},
     int year,month;
     do{
         printf("please input year,month:");
-        scanf("%d,%d",&year,&month);//yearºÍmonthÓÃ¶ººÅ¸ô¿ª
+        scanf("%d %d",&year,&month);
     }while(month<1||month>12);
     if ((0 == year % 4 && 0 != year % 100) || 0 == year % 400)
     {
