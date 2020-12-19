01d53dafa0847b61c4c921632907ca1eb554339e
LLP2333 <nasa105@foxmail.com>
2020-12-19 11:51:40 +0800

    nask

--- Pointer/9-5.c
+++ Pointer/9-5.c
@@ -6,13 +6,13 @@ int main(void)
 {
   int arg =1;
   printf("arg = %d\n",arg);
-  Fun(&arg);
+  Fun(&arg);/*将arg的地址传入函数*/
   printf("arg = %d\n",arg);
   system("pause");
   return 0;
 }
 void Fun(int *par)
 {
-    printf("par = %d\n",*par);
-    *par =2;
+    printf("par = %d\n",*par);/*输出形参指向的变量的值*/
+    *par =2;/*改变形参指向的变量的值*/
 }
