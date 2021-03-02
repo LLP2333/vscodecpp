41104e28f2f5da83d29d57f07a6e873155f0882b
LLP2333 <nasa105@foxmail.com>
2021-03-02 12:38:11 +0800

    yao

---
+++ 2021/2333.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main(void)
+{   
+    int n,k;
+    for(n=0;n<4;n++)
+    {
+    
+        
+            for(k=0;k<n;k++)
+            {
+                printf("  ");
+            }
+            printf("*****\n");
+        
+    }
+    system("pause");
+}
